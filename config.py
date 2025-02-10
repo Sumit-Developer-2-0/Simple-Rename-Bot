c6982364be32bf59f66f7f71053ebf55e3ed216a
Sumit-Developer-2-0 <sumitdeveloper2.0@gmail.com>
2025-02-10 23:52:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN = int(os.getenv("ADMIN", "0"))
-CAPTION = os.getenv("CAPTION", "")
+API_ID = int(os.getenv("API_ID", "26489431"))
+API_HASH = os.getenv("API_HASH", "9a2fce85339bb79254a55368a61ab92f")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7544054473:AAG9aWU4amrjzz-34x9nGKQX-kG3-EVEQxo")
+ADMIN = int(os.getenv("ADMIN", "7912527708"))
+CAPTION = os.getenv("CAPTION", "{file_name}\n\n💽 Size = {file_size}\n\n📯Join My Channel : @mkn_bots_updates")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
